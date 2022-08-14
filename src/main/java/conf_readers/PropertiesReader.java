c4576d8811059b0d765d8288104541574682de76
Stanislav Sinitsa <sin.ssj@yandex.ru>
2022-08-14 12:56:13 +0300

    не подтягивается пропертис

--- src/main/java/conf_readers/PropertiesReader.java
+++ src/main/java/conf_readers/PropertiesReader.java
@@ -4,18 +4,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class PropertiesReader implements IConfReaders {
 
     @Override
     public Map<String, String> read() {
-       // String rootDir = System.getProperty("user.dir");
+       String rootDir = System.getProperty("user.dir");
 
         Map<String, String> prop = new HashMap<>();
 
         try {
-        InputStream input = new FileInputStream("%s\\java\\resourses\\db.properties");
+        InputStream input = Files.newInputStream(Path.of(String.format("%s/src/java/resourses/%s", rootDir, "db.properties")));
             Properties properties = new Properties();
             properties.load(input);
 
